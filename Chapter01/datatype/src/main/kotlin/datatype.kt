a4d40f5a4a49788a656987b742e799415456747e
0525hhgus <0525_hhgus@naver.com>
2021-01-13 15:49:39 +0900

    20210113

---
+++ Chapter01/datatype/src/main/kotlin/datatype.kt
@@ -0,0 +1,7 @@
+fun main(){
+    var userName = "Kotlin"
+    val count = 5
+    userName = "Dwin"
+
+    println("username: $userName, count: $count")
+}
--- datatype/src/main/kotlin/datatype.kts
+++
@@ -1,7 +0,0 @@
-fun main() {
-    var username = "Kotlin" // 가변형 문자열
-    val count = 3 // 불변형 정수
-    username = "newKotlin"
-
-    println("username: $username, count: $count")
-}
